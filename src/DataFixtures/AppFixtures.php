87a1fb65de8450874529dd8aada52d6da24e81de
Frazew <frazew@minet.net>
2019-10-04 12:19:06 +0200

    Add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,47 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Owner;
+use App\Entity\Region;
+use App\Entity\Room;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    public const IDF_REGION_REFERENCE = 'idf-region';
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $region = new Region();
+        $region->setCountry("FR");
+        $region->setName("Ile de France");
+        $region->setPresentation("La région française capitale");
+        $manager->persist($region);
+
+        $manager->flush();
+
+        $this->addReference(self::IDF_REGION_REFERENCE, $region);
+
+        $owner = new Owner();
+        $owner->setFirstname("Patrick");
+        $owner->setFamilyName("Martin");
+        $owner->setAddress("1 rue de l'Église");
+        $owner->setCountry("FR");
+        $manager->persist($owner);
+
+        $manager->flush();
+
+        $room = new Room();
+        $room->setSummary("Beau poulailler ancien à Évry");
+        $room->setDescription("très joli espace sur paille");
+        $room->addRegion($this->getReference(self::IDF_REGION_REFERENCE));
+        $room->setCapacity(3);
+        $room->setSuperficy(82);
+        $room->setPrice(238);
+        $room->setOwner($owner);
+
+        $manager->persist($room);
 
         $manager->flush();
     }
