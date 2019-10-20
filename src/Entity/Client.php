6e760c7b65bb9dde9682b9a0cb268d84d68e8715
Frazew <frazew@minet.net>
2019-10-20 14:33:10 +0200

    Add client<->user relation

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -58,6 +58,12 @@ class Client
      */
     private $reservations;
 
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User", inversedBy="client", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
     public function __construct()
     {
         $this->reservations = new ArrayCollection();
@@ -179,6 +185,18 @@ class Client
                 $reservation->setClient(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
